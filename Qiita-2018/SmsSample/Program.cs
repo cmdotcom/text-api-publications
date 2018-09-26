ebd70a4ed656891fe006a7f5177fa58598b06d70
Joep Beusenberg <joep@bepost.nl>
2018-09-26 07:13:05 +0200

    Add sample for Qiita blog post (#2)

--- Qiita-2018/SmsSample/Program.cs
+++ Qiita-2018/SmsSample/Program.cs
@@ -1,8 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SmsSample
 {
@@ -29,22 +29,32 @@ namespace SmsSample
 
         private static async Task MainAsync()
         {
-            var request = new {
-                messages = new { 
-				    // プロダクトトークンはここから確認する: https://gateway.cmtelecom.com
+            var request = new
+            {
+                messages = new
+                {
+                    // プロダクトトークンはここから確認する: https://gateway.cmtelecom.com
                     authentication = new {producttoken = new Guid("ここにプロダクトトークンを入力")},
-                    msg = new[] { new {
-						// 送信者名に関しては下記＊１を確認
-                        from = "送信者名をここに入力",
-                        to = new[] { new {number = "+8180<ここに電話番号>"}},
-                        body = new { type = "AUTO", content = "これはテストSMSです！"}
-                    }}
+                    msg = new[]
+                    {
+                        new
+                        {
+                            // 送信者名に関しては下記＊１を確認
+                            from = "送信者名をここに入力",
+                            to = new[] {new {number = "+8180<ここに電話番号>"}},
+                            body = new {type = "AUTO", content = "これはテストSMSです！"}
+                        }
+                    }
                 }
             };
 
             using (var client = new HttpClient())
             {
-                var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var content = new StringContent(
+                    JsonConvert.SerializeObject(request),
+                    Encoding.UTF8,
+                    "application/json"
+                );
                 var response = await client.PostAsync("https://gw.cmtelecom.com/v1.0/message", content);
                 response.EnsureSuccessStatusCode();
                 Console.WriteLine(await response.Content.ReadAsStreamAsync());
